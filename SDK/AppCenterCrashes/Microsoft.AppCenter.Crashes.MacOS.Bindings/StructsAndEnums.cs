2a1c1f8c7b95e6a65cab8617cd934c2d20e9ef9e
Bailey Hu <3787826+apparentvisuals@users.noreply.github.com>
2018-03-06 10:56:12 -0500

    Add AppCenter MacOS Bindings for Analytics and Crashes based on iOS Bindings

---
+++ SDK/AppCenterCrashes/Microsoft.AppCenter.Crashes.MacOS.Bindings/StructsAndEnums.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ObjCRuntime;
+
+namespace Microsoft.AppCenter.Crashes.MacOS.Bindings {
+    [Native]
+    public enum MSErrorLogSetting : ulong
+    {
+        Disabled = 0,
+        AlwaysAsk = 1,
+        AutoSend = 2
+    }
+
+    [Native]
+    public enum MSUserConfirmation : ulong
+    {
+        DontSend = 0,
+        Send = 1,
+        Always = 2
+    }
+}
